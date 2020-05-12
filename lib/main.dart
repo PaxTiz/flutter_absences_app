109991ab6311d88677b2740b7e1b8624582cdc95
Valentin Cernuta <val.cernuta@gmail.com>
2020-05-12 21:19:59 +0200

    feat: ios splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -25,38 +25,29 @@ class Absences extends StatefulWidget {
 class AbsencesState extends State<Absences> {
 
 	int _currentIndex = 0;
-
 	final List<SemesterView> semesters = [
 		SemesterView(1),
 		SemesterView(2),
 		SemesterView(3),
 		SemesterView(4),
 	];
-
+	final List<BottomNavigationBarItem> navItems = [];
 
 	AbsencesState() {
 		createNavItems();
 	}
 
-	final List<BottomNavigationBarItem> navItems = [];
-
 	void createNavItems() {
 		for(int i = 1; i < semesters.length + 1; i++) {
 			IconData icon;
-			switch(i) {
-				case 1:
-					icon = Icons.filter_1;
-					break;
-				case 2:
-					icon = Icons.filter_2;
-					break;
-				case 3:
-					icon = Icons.filter_3;
-					break;
-				case 4:
-					icon = Icons.filter_4;
-					break;
-			}
+			if (i == 1)
+				icon = Icons.filter_1;
+			else if (i == 2)
+				icon = Icons.filter_2;
+			else if (i == 3)
+				icon = Icons.filter_3;
+			else if (i == 4) icon = Icons.filter_4;
+
 			navItems.add(BottomNavigationBarItem(
 				icon: Icon(icon, color: Colors.black,),
 				title: Text("Semestre $i", style: TextStyle(color: Colors.black),)
